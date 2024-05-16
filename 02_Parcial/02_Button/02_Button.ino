7e2b342e03c783c6caf93faf6043a689c438531d
MiguelAtila <cdmx2975@amerike.edu.mx>
2024-05-16 11:17:17 -0600

    modificación código arduino boton

--- 02_Parcial/02_Button/02_Button.ino
+++ 02_Parcial/02_Button/02_Button.ino
@@ -1,5 +1,6 @@
 const int inputPin =7;
 int input7=0;
+int lastState=0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,13 +11,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   input7 = digitalRead(inputPin);
-  if(input7 == HIGH)
+  if(lastState == LOW && input7 == HIGH)
   {
     Serial.println("1");
   }
-  else
+  else if (lastState == HIGH && input7 == LOW)
   {
     Serial.println("0");
   }
+  lastState = input7;
   delay(100);
 }
